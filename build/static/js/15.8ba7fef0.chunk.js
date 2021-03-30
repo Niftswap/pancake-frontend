325e412579d7f2d5e84f4440a7c454e0e88ca3d5
umar-shafiq <umarshafiq2012@gmail.com>
2021-03-30 17:40:15 +0500

    build added

---
+++ build/static/js/15.8ba7fef0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[15],{1108:function(n,e,a){"use strict";a.r(e);var t=a(1099);e.default=t.Line}}]);
+//# sourceMappingURL=15.8ba7fef0.chunk.js.map
---
+++ build/static/js/17.8665d564.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[17],{959:function(n,p){}}]);
+//# sourceMappingURL=17.8665d564.chunk.js.map
